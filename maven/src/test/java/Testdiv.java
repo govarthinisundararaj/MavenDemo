c289c2bf84ac7c4b0e2180613d8248652c6a4f8b
govarthinisundararaj <35028923+govarthinisundararaj@users.noreply.github.com>
2020-02-18 19:31:22 +0530

    Update Testdiv.java

--- maven/src/test/java/Testdiv.java
+++ maven/src/test/java/Testdiv.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import junit.framework.Assert;
-import main.java.Add;
-
+import main.java.Task1;
 
 public class Testdiv {
 	@Test
 	public void div_fun()
 	{
-		Add a=new Add();
-		Assert.assertEquals(1, a.div_10(5,3,2));
+		Task1 a=new Task1();
+		Assert.assertEquals(1, a.div_10(5,4,2));
 		
 }
+
 }
